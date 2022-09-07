80d20c621c940534f3870a7422a5d771383728c8
Pattarakorn Kuncharoen <pattarakorn.kunc@kmutt.ac.th>
2022-09-07 16:28:49 +0700

    Yeahh!

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,8 +2,30 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
+  return weight / (height*height)
 }
 function getBMIMeaning(weight, height) {
   //code here
+  // let bmi = calculateBMI()
+  // switch (bmi){
+  //   case bmi < 18.5 :
+  //     console.log('Underweight')
+  //     break
+  //   case bmi > 18.5 && bmi <= 24.9 :
+  //     console.log('Normal weight')
+  //     break
+  //   case bmi > 25:
+  //     console.log('Overweight')
+  //     break
+  // }
+
+  let bmi = calculateBMI(weight,height)
+      if (bmi < 18.5){
+            return'Underweight'
+      }else if (bmi >= 18.5 && bmi < 24.9){ 
+            return 'Normal weight'
+      }else{
+            return'Overweight'
+      }
 }
 module.exports = getBMIMeaning
